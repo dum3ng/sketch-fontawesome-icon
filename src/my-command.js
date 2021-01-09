8731780045574a0d785acbd7538cc4fd1a916c59
dum3ng <dumeng81@outlook.com>
2021-01-09 22:43:05 +0800

    add types

--- src/my-command.js
+++ src/my-command.js
@@ -1,6 +1,13 @@
 import sketch from 'sketch'
 // documentation: https://developer.sketchapp.com/reference/api/
 
-export default function() {
-  sketch.UI.message("It's alive 🙌")
+export default function () {
+  // sketch.UI.message("It's alive 🙌")
+  const doc = sketch.getSelectedDocument()
+  const page = doc.selectedPage
+  const { Text, Rectangle } = sketch
+  const text = new Text()
+  text.text = 'hello plugin'
+  text.parent = page
+  text.frame = new Rectangle(100, 100)
 }
